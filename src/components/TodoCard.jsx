dc4e782931bd31c807d0e4b1f8e53941cbd2782b
tratchapong <tratchapong@gmail.com>
2024-02-08 13:50:10 +0700

    TodoCard bg-color base on status

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 export default function TodoCard(props) {
   const {el, openModal} = props
+  let statusColor = el.status==='PENDING' ? 'bg-pink-200' : el.status==='DOING' ? 'bg-blue-200' : 'bg-lime-200'
   return (
-    <div className="card w-4/5 bg-pink-200 shadow-xl cursor-pointer 
-      active:shadow-none active:translate-x-1 active:translate-y-1"
+    <div className={`card w-4/5 shadow-xl cursor-pointer 
+      active:shadow-none active:translate-x-1 active:translate-y-1 ${statusColor}`}
       onClick={()=>openModal(el.id)}
     >
       <div className="card-body">
